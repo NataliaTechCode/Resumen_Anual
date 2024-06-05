86883a0b1acd171cc81c61b650f63b0f2c59de49
unknown <nataliespinoza16.02@gmail.com>
2024-06-04 22:55:53 -0400

    nuevos cambios

--- .netlify/functions-internal/ssr/chunks/generic_WWajcAde.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_DW0fadVn.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_BSgpKr-i.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_DKrf-Yx5.mjs');
-
-export { page };
