3e56c9bde75ea151a49924ead95e5236198e2b12
unknown <nataliespinoza16.02@gmail.com>
2024-06-04 22:56:53 -0400

    nuevos cambios

---
+++ .netlify/functions-internal/ssr/chunks/iniciar_sesion_B6ODGLNM.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/iniciar_sesion_dLfa1T6y.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/ssr/chunks/pages/index_gKvVtwax.mjs
@@ -0,0 +1,45 @@
+function render({ slots: ___SLOTS___ }) {
+		return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="Un módulo para visualizar las estadisticas de Arbu"
+    />
+    <title>Arbu</title>
+    <meta name="title" content="Arbu" />
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://arbu-share.netlify.app" />
+    <meta property="og:title" content="Arbu" />
+    <meta
+      property="og:description"
+      content="Un módulo para visualizar las estadisticas de Arbu"
+    />
+    <meta
+      property="og:image"
+      content="https://firebasestorage.googleapis.com/v0/b/arbu-c574d.appspot.com/o/logo%20arbu%20og.png?alt=media&token=05f46213-a773-428a-b3fc-50cf611ac0bb"
+    />
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://arbu-share.netlify.app" />
+    <meta property="twitter:title" content="Arbu" />
+    <meta
+      property="twitter:description"
+      content="Un módulo para visualizar las estadisticas de Arbu"
+    />
+    <meta
+      property="twitter:image"
+      content="https://firebasestorage.googleapis.com/v0/b/arbu-c574d.appspot.com/o/logo%20arbu%20og.png?alt=media&token=05f46213-a773-428a-b3fc-50cf611ac0bb"
+    />
+  </head>
+  <body></body>
+</html>
+`
+	}
+render["astro:html"] = true;
+
+export { render as default };
