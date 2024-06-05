8fe90467d1ea72743041fccc4dd127b789ab67a0
unknown <nataliespinoza16.02@gmail.com>
2024-06-04 21:32:02 -0400

    nuevos cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [react()]
 });
