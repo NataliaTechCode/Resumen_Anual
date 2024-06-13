bab48488ca49121dac75cdbe5a990c504dd3b23b
unknown <nataliespinoza16.02@gmail.com>
2024-06-12 20:52:40 -0400

    nuevos cambios

--- src/services/loadScoreGlobal.js
+++ src/services/loadScoreGlobal.js
@@ -2,10 +2,10 @@ import  {db} from "../firebase/client"
 import {collection, getDocs,query} from "firebase/firestore";
 
 export const loadScoreGlobal = async () => {
-  const q = query(collection(db, 'ranking'));
+  // const q = query(collection(db, 'ranking'));
 
-  const querySnapshot = await getDocs(q);
-  const scoreGlobal = querySnapshot.docs.map((doc) => doc.data());
-  return scoreGlobal;
-  // return [];
+  // const querySnapshot = await getDocs(q);
+  // const scoreGlobal = querySnapshot.docs.map((doc) => doc.data());
+  // return scoreGlobal;
+  return [];
 };
